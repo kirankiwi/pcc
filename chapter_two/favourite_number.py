b99f6d1906a69fee71161651289de2cc4c5a27bc
Kiran Keswani <kirangkeswani@gmail.com>
2024-04-23 13:35:06 +1200

    Completed integer activities and added comments to some programs.

---
+++ chapter_two/favourite_number.py
@@ -0,0 +1,4 @@
+favourite_number = "37"
+# My favourite number
+print(f"My favourite number is {favourite_number}!")
+print(0.2+0.1)
---
+++ chapter_two/number_eight.py
@@ -0,0 +1,4 @@
+print(7+1)
+print(24/3)
+print(16*0.5)
+print(564-556)
