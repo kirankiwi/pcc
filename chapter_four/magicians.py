24263b1efab2278e11edf442b74d97c852d39759
Kiran Keswani <kirangkeswani@gmail.com>
2025-03-19 18:16:54 +1300

    Began chapter four.

---
+++ chapter_four/magicians.py
@@ -0,0 +1,46 @@
+magicians = ["David","Alice","Bob","Daniel","Thomas","Joe"]
+print("Here are the magicians:\n")
+for magician in magicians:
+    print(magician)
+print("\nHere are the magicians in alphabetical order.\n")
+for magician in sorted(magicians):
+    print(magician)
+print("\nOn to the magic show!")
+for magician in magicians:
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    print(f"\nWow, {magician}, that was an amazing trick!")
+    print("I can't wait to see your next one!\n")
+print("\nThank you all, that was amazing!")
