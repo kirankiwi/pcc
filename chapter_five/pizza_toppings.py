2c628df7020482b414144312c0a9cb68b570e269
Kiran Keswani <kirangkeswani@gmail.com>
2026-04-23 13:57:56 +1200

    Started and finished chapter five

---
+++ chapter_five/pizza_toppings.py
@@ -0,0 +1,49 @@
+import time
+
+available_toppings = ["Pizza sauce", "Mushroom", "Cheese", "Olives", "Zucchini", "Capsicu"
+"m", "Pineapple", "Onion", "Peperoni"]
+
+pizza_toppings = []
+
+print("\n")
+
+if not pizza_toppings:
+    print("Are you sure you want a plain pizza?")
+    print("\n")
+
+for topping in pizza_toppings:
+    if topping not in available_toppings:
+        print(f"Sorry, we don't have any {topping}.")
+
+if "Pizza sauce" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding pizza sauce to your pizza!")
+if "Mushroom" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding mushrooms to your pizza!")
+if "Cheese" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding cheese to your pizza!")
+if "Zucchini" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding zucchini to your pizza!")
+if "Olives" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding olives to your pizza!")
+if "Capsicum" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding capsicum to your pizza!")
+if "Pineapple" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding pineapple to your pizza!")
+if "Onion" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding onion to your pizza!")
+if "Peperoni" in pizza_toppings:
+    time.sleep(0.5)
+    print("Adding peperoni to your pizza!")
+
+if pizza_toppings:
+    time.sleep(1)
+    print("\nYour pizza is ready!\n")
+    time.sleep(2)
---
+++ chapter_five/stages_of_life.py
@@ -0,0 +1,16 @@
+age = 423845987402938
+
+if age < 2:
+    print("The person is a baby!")
+elif age < 4:
+    print("The person is a toddler!")
+elif age < 13:
+    print("The person is a child!")
+elif age < 20:
+    print("The person is a teenager!")
+elif age < 65:
+    print("The person is an adult!")
+elif age < 100:
+    print("The person is an elder!")
+else:
+    print("\033[31mWow!\033[0m This person is \033[33mANCIENT!\033[0m")
