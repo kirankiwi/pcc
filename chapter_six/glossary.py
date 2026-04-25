b51ae4b4fe6b80695821d252e9ad02bc0323a68b
Kiran Keswani <kirangkeswani@gmail.com>
2026-04-25 18:01:03 +1200

    Completed chapter six

---
+++ chapter_six/glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    "Python": "The best programming language!",
+    "Integer": "A number with no decimal places.",
+    "Float": "A number with decimal places.",
+    "Git": "A popular version control system.",
+    "Syntax": "The structure of how you write a programming language."
+}
+
+for word, definition in glossary.items():
+    print(f"\n{word}:\n\t{definition}")
---
+++ chapter_six/person.py
@@ -0,0 +1,11 @@
+person = {
+    "first_name": "Nitin",
+    "last_name": "Keswani",
+    "age": 15,
+    "city": "Christchurch"
+
+}
+
+print(f"{person["first_name"]} {person['last_name']} is {person['age']} years o"
+      f"ld and lives in {person['city']}."
+      )
